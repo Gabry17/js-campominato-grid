04d5887cc3ad6d397c46d8c908ce048f6de29694
Gabry17 <gbianchi528@gmail.com>
2022-04-23 15:26:52 +0200

    html: aggiunto titolo js: aggiunto livelli di difficolta con condizione e aggiunto griglia al click

--- js/script.js
+++ js/script.js
@@ -1,21 +1,49 @@
 
-//creare griglia da 1 a 100
-//passare arg alla function
-const newArray = gridGame(100);
-console.log(newArray);
+const btnPlay = document.querySelector('button');
+btnPlay.addEventListener('click', play);
+
+function play(){
+//al click aggiungere class container
+const elemCont = document.querySelector('main > div');
+elemCont.classList.add('container');
+
+//selezionare valori dell input
+let valInput = document.querySelector('select').value;
+console.log(valInput);
+//passare arg alla funzione a seconda del valore selezionato
+let newArray;
+
+if(valInput === 'facile'){
+    newArray = gridGame(100);
+} else if (valInput === 'avanzato') {
+    newArray = gridGame(80);
+} else if (valInput === 'difficile') {
+    newArray = gridGame(50)
+}
+//creare griglia
 //prendere elemento griglia html 
 const container = document.querySelector('.container');
+container.innerHTML = "";
+
 //creare ciclo for e gli passiamo l'array
 for(let i = 0; i < newArray.length; i++){
     const arrayI = newArray[i];
     const div = document.createElement('div');
     div.innerHTML = `<span>${arrayI}</span>`;
-    div.classList.add('square')
+    div.classList.add('square');
     container.append(div);
+
+    //al click di ogni casella aggiungere classe bg-blue
+    div.addEventListener('click', function(){
+        this.classList.add('bg-blue');
+    })
+
+    container.append(div);
+
+}
 }
 
 
-//al click di ogni casella aggiungere classe bg-blue
 
 
 //FUNCTION
